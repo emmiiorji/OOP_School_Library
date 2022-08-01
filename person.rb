2d15f36ee14fa50e1c432fefda142ef88ce233a7
emmiiorji <emmanuelorji2013@gmail.com>
2022-08-01 08:00:41 +0100

    Use attr_accessor, attr_reader to define trivial reader & writer methods

--- person.rb
+++ person.rb
@@ -1,27 +1,11 @@
 class Person
-  def initialize(age, name = 'Unknown', parent_permission = true)
+  attr_accessor :age, name
+  attr_reader :id
+
+  def initialize(age, name: 'Unknown', parent_permission: true)
     @age = age
     @name = name
     @parent_permission = parent_permission
   end
 
-  def id
-    @id
-  end
-
-  def name
-    @name
-  end
-  
-  def age
-    @age
-  end
-
-  def name=(name)
-    @name = name
-  end
-
-  def age=(age)
-    @age = age
-  end
 end
