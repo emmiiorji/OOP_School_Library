ed2aa9b681f95f19d56dbec2f44fc8bcad585cfa
emmiiorji <emmanuelorji2013@gmail.com>
2022-08-01 10:23:17 +0100

    Use keyword argument for boolean parent_permission argument

--- person.rb
+++ person.rb
@@ -2,7 +2,7 @@ class Person
   attr_accessor :age, name
   attr_reader :id
 
-  def initialize(age, name: 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @age = age
     @name = name
     @parent_permission = parent_permission
