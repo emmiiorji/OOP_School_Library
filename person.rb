cbed5bc71e4a9c053c9770935239d4f64c46464c
emmiiorji <emmanuelorji2013@gmail.com>
2022-08-23 21:13:10 +0100

    Create base component Nameable

--- person.rb
+++ person.rb
@@ -1,3 +1,9 @@
+class Nameable
+  def correct_name
+    raise NotImplementedError, "#{self.class} has not implemented method #{__method__}"
+  end
+end
+
 class Person
   attr_accessor :age, name
   attr_reader :id
