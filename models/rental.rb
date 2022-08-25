f97c6cfce89bfdd26083e0cd96b178405befacb1
emmiiorji <emmanuelorji2013@gmail.com>
2022-08-25 08:44:12 +0100

    Add belongs-to relationship between Rental and Person

--- models/rental.rb
+++ models/rental.rb
@@ -1,9 +1,12 @@
 class Rental
   attr_accessor :date
-  attr_reader :book
+  attr_reader :book, :person
 
   def initialize(person, book, date)
     @date = date
+
+    @person = person
+    person.rentals << self
 
     @book = book
     book.rentals << self
