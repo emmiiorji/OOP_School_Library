c57e7c992c3d45da274d94ba8f23f1a5ec90430f
emmiiorji <emmanuelorji2013@gmail.com>
2022-08-27 08:42:22 +0100

    Define function in App class to create book

--- models/app.rb
+++ models/app.rb
@@ -70,4 +70,14 @@ class App
     @persons.push(Teacher.new(age, specialization, name))
     "Teacher created successfully"
   end
+
+  def create_book
+    print "Title: "
+    title = gets.chomp
+
+    print "Author: "
+    author = gets.chomp
+    @books.push(Book.new(title, author))
+    puts "Book created successfully"
+  end
 end
