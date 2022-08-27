42a401ceecf5d602afe8d84f5cf09b244ef48b77
emmiiorji <emmanuelorji2013@gmail.com>
2022-08-27 09:10:09 +0100

    Change double quote strings having no interpolation to single quotes

--- models/app.rb
+++ models/app.rb
@@ -1,7 +1,7 @@
-require_relative "./student.rb"
-require_relative "./teacher.rb"
-require_relative "./classroom.rb"
-require_relative "./rental.rb"
+require_relative './student'
+require_relative './teacher'
+require_relative './classroom'
+require_relative './rental'
 
 class App
   attr_accessor :classrooms
@@ -11,81 +11,81 @@ class App
     @books = []
     @rentals = []
     @classrooms = []
-    create_classrooms("Class1") #Create some classroom
+    create_classrooms('Class1') # Create some classroom
   end
 
   def create_classrooms(label = nil)
-    puts "Enter classroom label" unless label
+    puts 'Enter classroom label' unless label
     label = gets.chomp unless label
     @classrooms.push(Classroom.new(label)) unless @classrooms.filter {|classroom| classroom.label === label}.length > 0
   end
 
   def create_person
-    print "Do you want to create a student(1) or a teacher (2)? [Enter the number]: "
+    print 'Do you want to create a student(1) or a teacher (2)? [Enter the number]: '
     create_choice = gets.chomp
     case create_choice
-    when "1"
+    when '1'
       create_student
-    when "2"
+    when '2'
       create_teacher
     else
-      puts "You have made an invalid choice. Exiting..."
+      puts 'You have made an invalid choice. Exiting...'
       return
     end
   end
 
   def create_student
     begin
-      print "Age: "
+      print 'Age: '
       age = Float(gets.chomp)
     rescue ArgumentError
-      puts "Invalid value for age. Exiting..."
-      #Return to the beginning
+      puts 'Invalid value for age. Exiting...'
+      # Return to the beginning
     end
 
-    print "Name: "
+    print 'Name: '
     name = gets.chomp
 
-    print "Has parent permission? [Y/N]: "
-    permission = gets.chomp.downcase === "y"
+    print 'Has parent permission? [Y/N]: '
+    permission = gets.chomp.downcase === 'y'
     @persons.push(Student.new(age, @classrooms[0], name, parent_permission: permission))
-    "Student created successfully"
+    'Student created successfully'
   end
 
   def create_teacher
     begin
-      print "Age: "
+      print 'Age: '
       age = Float(gets.chomp)
     rescue ArgumentError
-      puts "Invalid value for age. Creating teacher was unsuccessful..."
+      puts 'Invalid value for age. Creating teacher was unsuccessful...'
       return
-      #Return to the beginning
+      # Return to the beginning
     end
 
-    print "Name: "
+    print 'Name: '
     name = gets.chomp
 
-    print "Specialization: "
-    specialization = gets.chomp.downcase === "y"
+    print 'Specialization: '
+    specialization = gets.chomp.downcase === 'y'
     @persons.push(Teacher.new(age, specialization, name))
-    "Teacher created successfully"
+    'Teacher created successfully'
   end
 
   def create_book
-    print "Title: "
+    print 'Title: '
     title = gets.chomp
 
-    print "Author: "
+    print 'Author: '
     author = gets.chomp
     @books.push(Book.new(title, author))
-    puts "Book created successfully"
+    puts 'Book created successfully'
   end
 
   def list_all_people
     puts "\nNo one found" if @persons.empty?
     @persons.each { |person| puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"}
   end
-  
+
   def list_all_books
     puts "\nNo book found" if @books.empty?
     @books.each { |book| puts "Title: \"#{book.title}\", Author: #{book.author}"}
@@ -93,8 +93,8 @@ class App
 
   def create_rental
     puts "\n\nSelect a book from the following list by number"
-    @books.each_with_index { |book, index| puts "Title: \"#{book.title}\", Author: #{book.author}"}
-    puts "Choice book: "
+    @books.each_with_index { |book, _index| puts "Title: \"#{book.title}\", Author: #{book.author}" }
+    puts 'Choice book: '
     begin
       choice_book_index = Integer(gets.chomp)
       unless (0...@books.length).member?(choice_book_index)
@@ -104,14 +104,14 @@ class App
     rescue ArgumentError
       puts "Invalid value for book index.\nCreating rental unsuccessful..."
       return
-      #Return to the beginning
+      # Return to the beginning
     end
-    
+
     puts "\n\nSelect a person from the following list by number (not id)"
     @persons.each_with_index { |person, index|
       puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
     }
-    puts "Choice book: "
+    puts 'Choice book: '
     begin
       choice_person_index = Integer(gets.chomp)
       unless (0...@persons.length).member?(choice_person_index)
@@ -123,27 +123,26 @@ class App
       return
     end
 
-    puts "Date [YYYY/MM/DD]: "
+    puts 'Date [YYYY/MM/DD]: '
     rent_date = gets.chomp
     begin
-      t_date = Time.strptime(rent_date, "%Y/%m/%d")
-      if (t_date.strftime("%Y/%m/%d"))
+      t_date = Time.strptime(rent_date, '%Y/%m/%d')
+      if t_date.strftime('%Y/%m/%d')
         rentals.push(Rental.new(persons[choice_person_index, books[choice_book_index, rent_date]]))
-        puts "Rental created succssfully"
+        puts 'Rental created succssfully'
       end
     rescue ArgumentError
       puts "Invalid value for date\nCreating rental was unsuccessful..."
       return
     end
-    
   end
 
   def get_rental_by_id
-    puts "ID of person: "
+    puts 'ID of person: '
     begin
       person_id = Integer(gets.chomp)
     rescue ArgumentError
-      puts "ID must be a valid integer"
+      puts 'ID must be a valid integer'
       return
     end
 
--- models/main.rb
+++ models/main.rb
@@ -1,13 +1,13 @@
-require_relative "./app.rb"
+require_relative './app'
 
 def display_home_choices
   choices = [
-    "List all books", "List all people", "Create a person", "Create a book",
-    "Create a rental", "List all rentals for a given person id", "Exit"
+    'List all books', 'List all people', 'Create a person', 'Create a book',
+    'Create a rental', 'List all rentals for a given person id', 'Exit'
   ]
   puts "\n\nPlease choose an action by entering the option number."
   choices.each_with_index { |choice, index| puts "#{index + 1} - #{choice}" }
-  print "Choice: "
+  print 'Choice: '
 end
 
 def main
@@ -15,39 +15,38 @@ def main
   display_home_choices
 
   app = App.new
-  
-  
+
   trials = 0
   loop do
     choice = gets.chomp
     case choice
-    when "1"
+    when '1'
       app.list_all_books
       display_home_choices
-    when "2"
+    when '2'
       app.list_all_people
       display_home_choices
-    when "3"
+    when '3'
       app.create_person
-      display_home_choices    
-    when "4"
+      display_home_choices
+    when '4'
       app.create_book
       display_home_choices
-    when "5"
+    when '5'
       app.create_rental
       display_home_choices
-    when "6"
+    when '6'
       app.get_rental_by_id
       display_home_choices
-    when "7"
+    when '7'
       puts "\nThank you for using this app. Bye!"
       return
     else
       trials += 1
-      puts "You've entered an invalid character#{trials > 2? " three times. Bye" : ""}!" 
-      print "Choice: "
+      puts "You've entered an invalid character#{trials > 2 ? ' three times. Bye' : ''}!"
+      print 'Choice: '
     end
-  break if trials > 2
+    break if trials > 2
   end
 end
 
