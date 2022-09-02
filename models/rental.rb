2fac800de3f7aba8c1eb9a5fdfa0af63eae1a535
emmiiorji <emmanuelorji2013@gmail.com>
2022-09-02 16:44:25 +0100

    Add to_hash method in Rental class

--- models/rental.rb
+++ models/rental.rb
@@ -11,4 +11,12 @@ class Rental
     @book = book
     book.rentals << self
   end
+
+  def to_hash
+    {
+      date: @date,
+      person: @person.to_hash,
+      book: @book.to_hash
+    }
+  end
 end
