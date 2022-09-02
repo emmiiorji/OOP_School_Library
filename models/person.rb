e57be8a9d36db2149667395ddb92991ac8a8129f
emmiiorji <emmanuelorji2013@gmail.com>
2022-09-02 18:12:34 +0100

    Modify Person initialize method to receive id optionally

--- models/person.rb
+++ models/person.rb
@@ -7,13 +7,13 @@ class Person < Nameable
   attr_accessor :age, :name
   attr_reader :id, :rentals
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', id = nil, parent_permission: true)
     super()
     @age = age
     @name = name
     @parent_permission = parent_permission
     @rentals = []
-    @id = ((rand * 100_000) + (rand * 100_000)).ceil
+    @id = id || ((rand * 100_000) + (rand * 100_000)).ceil
   end
 
   def can_use_services?
@@ -28,5 +28,15 @@ class Person < Nameable
 
   def of_age?
     @age >= 18
+  end
+
+  def to_hash
+    {
+      age: @age,
+      name: @name,
+      id: @id,
+      parent_permission: @parent_permission,
+      rentals: @rentals.map(&:to_hash)
+    }
   end
 end
